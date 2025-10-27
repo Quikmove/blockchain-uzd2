21b01f53c7419dd2c62f8ae3113e38894629761a
Kristupas Arifovas <50778281+Quikmove@users.noreply.github.com>
2025-10-27 23:32:05 +0200

    feat: implement function to generate fund transactions for users

---
+++ internal/blockchain/generate_funds.go
@@ -0,0 +1,53 @@
+package blockchain
+
+import (
+	"math/rand"
+)
+
+// go through users and select a random amount between 100 and 1000 to get transferred as UTXOs in exponential sizes: 1, 2, 4, 8, 16, etc.
+// afterwards, construct the genesis block - that's the only block with coinbase-like transactions
+
+func GenerateFundTransactionsForUsers(users []User, low, high uint32) (Transactions, error) {
+	var txs Transactions
+	for _, usr := range users {
+		var amount uint32
+		if high <= low {
+			amount = low
+		} else {
+			delta := int(high - low + 1)
+			if delta <= 0 {
+				amount = low
+			} else {
+				amount = low + uint32(rand.Intn(delta))
+			}
+		}
+		utxos := []uint32{}
+		remaining := amount
+		size := uint32(1)
+		for remaining > 0 {
+			if remaining >= size {
+				utxos = append(utxos, size)
+				remaining -= size
+				size *= 2
+			} else {
+				utxos = append(utxos, remaining)
+				remaining = 0
+			}
+		}
+		var outputs []TxOutput
+		for _, v := range utxos {
+			txOut := TxOutput{
+				Value: v,
+				To:    usr.PublicKey,
+			}
+			outputs = append(outputs, txOut)
+		}
+		tx := Transaction{
+			Inputs:  nil,
+			Outputs: outputs,
+		}
+		tx.TxID = tx.Hash()
+		txs = append(txs, tx)
+	}
+	return txs, nil
+}
