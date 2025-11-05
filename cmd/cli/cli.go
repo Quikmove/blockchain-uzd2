8768a673d4de5ae4a2b4c04f8eff8476f05d38be
Kristupas Arifovas <50778281+Quikmove@users.noreply.github.com>
2025-11-05 23:58:01 +0200

    feat: enhance CLI to generate and add random transactions to the blockchain

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/Quikmove/blockchain-uzd2/internal/blockchain"
 	"github.com/Quikmove/blockchain-uzd2/internal/config"
@@ -44,16 +45,62 @@ func main() {
 				Name:  "local",
 				Usage: "Start an interactive blockchain session",
 				Action: func(ctx context.Context, c *cli.Command) error {
+					hasher := blockchain.NewArchasHasher()
 					cfg := config.LoadConfig()
 					log.Println("Version:", cfg.Version)
 					log.Println("Difficulty:", cfg.Difficulty)
 					names := filetolist.FileToList(cfg.NameListPath)
-					users := blockchain.GenerateUsers(names, 3)
+					users := blockchain.GenerateUsers(names, 50)
 					log.Println("Generating genesis block...")
-					bch := blockchain.InitBlockchainWithFunds(100, 1000000, users, cfg, blockchain.NewArchasHasher())
+					bch := blockchain.InitBlockchainWithFunds(100, 1000000, users, cfg, hasher)
 					genesis, _ := bch.GetLatestBlock()
 					log.Println("Found a POW hash successfully with nonce:", genesis.Header.Nonce)
-
+					if err != nil {
+						log.Println(err)
+					}
+					txsSize := 100
+					totalTxs := 10_000
+					blocksToAdd := totalTxs / txsSize
+					for _ = range blocksToAdd {
+						txs, err := bch.GenerateRandomTransactions(users, 10, 50, txsSize)
+						if err != nil {
+							log.Println("Error generating transactions:", err)
+							continue
+						}
+						latestBlock, err := bch.GetLatestBlock()
+						if err != nil {
+							log.Println("Error getting latest block:", err)
+							continue
+						}
+						hash, err := bch.CalculateHash(latestBlock)
+						if err != nil {
+							log.Println("Error calculating previous block hash:", err)
+							continue
+						}
+						var header blockchain.Header
+						var body blockchain.Body
+						body.Transactions = txs
+						header.Version = cfg.Version
+						header.MerkleRoot = body.MerkleRootHash(hasher)
+						header.PrevHash = hash
+						header.Timestamp = uint32(time.Now().Unix())
+						header.Difficulty = cfg.Difficulty
+						nonce, _, err := header.FindValidNonce(ctx, hasher)
+						if err != nil {
+							log.Println("Error finding valid nonce:", err)
+							continue
+						}
+						header.Nonce = nonce
+						newBlock := blockchain.Block{
+							Header: header,
+							Body:   body,
+						}
+						if err := bch.AddBlock(newBlock); err != nil {
+							log.Println("Error adding new block to blockchain:", err)
+							continue
+						}
+						log.Println("Added new block with nonce:", newBlock.Header.Nonce)
+					}
 					return nil
 				},
 			},
